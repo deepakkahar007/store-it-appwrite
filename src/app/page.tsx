3c6d6ff6b1231add71edeabc22388ccdf2a2570b
deepakkahar007 <kahardeepak14@gmail.com>
2025-07-02 22:22:01 +0530

    shadcn ui added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>drive clone</h1>
+
+      <Button>Test</Button>
     </div>
   );
 }
